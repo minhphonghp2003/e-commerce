87c4a0a04bc8f8900b45955a1be0f8705414deb6
minhphonghp2003 <minhphonghp2003@gmail.com>
2022-08-30 15:00:00 +0700

    n

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import { Express } from "express";
+import express from "express";
+
+const app = express()
+const port = 3000
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.post('/', (req, res) => {
+    let req_body = req.body
+    res.send(req_body)
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
