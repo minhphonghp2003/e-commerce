b37662938e104dcbd6c7db5782d0736ac36d4a90
minhphonghp2003 <minhphonghp2003@gmail.com>
2022-09-01 12:35:40 +0700

    n

---
+++ test.js
@@ -0,0 +1,13 @@
+import pkg from 'pg';
+import 'dotenv/config'
+const {Pool} = pkg;
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+  })
+
+export {pool}
