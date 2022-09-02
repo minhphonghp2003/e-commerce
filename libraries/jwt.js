21b9c3c09d41adc8eedea32f31dac1434f53fe5f
minhphonghp2003 <minhphonghp2003@gmail.com>
2022-09-02 11:46:30 +0700

    n

--- libraries/jwt.js
+++ libraries/jwt.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 import fs from 'fs'
 
-let privateKey = fs.readFileSync('private.key')
+let privateKey = fs.readFileSync('/etc/secrets/private.key')
 
 const keygen =async (id, role) => {
     let token = jwt.sign({'id' : id, 'role' : role},privateKey,{ algorithm: 'RS256' },{ expiresIn: '1h' })
