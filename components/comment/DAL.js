46c4fe4d7e280552644b03e1aa81b58f9d0b7e10
minhphonghp2003 <minhphonghp2003@gmail.com>
2022-09-14 18:19:50 +0700

    n

--- components/comment/DAL.js
+++ components/comment/DAL.js
@@ -44,7 +44,9 @@ const updateComment = async (comment) => {
 }
 
 const getComment = async (product_id)=>{
-  let comments = (await pool.query("select c.id as cid,date,content,rate, u.fullname, u.id as uid from public.comment as c join public.user u on c.customer_id = u.id where product_id = $2",[product_id])).rows
+  console.log(product_id);
+  let comments = (await pool.query("select c.id as cid,date,content,rate, u.fullname, u.id as uid  from public.comment as c join public.user u on c.customer_id = u.id where c.product_id = $1",[product_id])).rows
+  
   return comments
 }
 
