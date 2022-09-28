53fd1800480ea4e6a72cc4aa7b7c94a1e23ec138
minhphonghp2003 <minhphonghp2003@gmail.com>
2022-09-28 08:04:44 +0700

    n

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers")
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+  res.setHeader("Access-Control-Allow-Headers", "*")
   next();
 });
 app.use('/user', user.router)
